31e5960ed89b69c78ddb8f16716b8a4390e1efdc
Simon Boigelot <sboigelot@gmail.com>
2021-09-26 15:05:53 +0200

    TwitchTest

---
+++ TwitchTest/Assets/SecretTemplate.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TwitchLib.Unity;
+
+public static class SecretTemplate
+{
+	public static string ClientId = "";
+	public static string ClientSecret = "";
+	public static string BotAccessToken = "";
+	public static string BotRefreshToken = "";
+	public static string ChannelId = "";
+}
